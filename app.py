c9731c6ec5d35b05f41ac3b26b750bfa3634042e
https://github.com/sunnysharma16685/loan-saathi-hub <75515374+sunnysharma16685@users.noreply.github.com>
2025-07-28 13:39:22 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1,27 +1,32 @@
-
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
     return redirect(url_for('login'))
 
-@app.route('/signup')
-def signup():
-    return render_template('signup.html')
-
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        return redirect(url_for('dashboard'))
     return render_template('login.html')
 
-@app.route('/forgot')
-def forgot():
-    return render_template('forgot.html')
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        return redirect(url_for('login'))
+    return render_template('signup.html')
 
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
 
-if __name__ == '__main__':
+@app.route('/forgot', methods=['GET', 'POST'])
+def forgot():
+    if request.method == 'POST':
+        return redirect(url_for('login'))
+    return render_template('forgot.html')
+
+if __name__ == "__main__":
     app.run(debug=True)

f5a478fc7a745d2c882a447b1cd8c058d0a9e681
https://github.com/sunnysharma16685/loan-saathi-hub <75515374+sunnysharma16685@users.noreply.github.com>
2025-08-18 16:37:46 +0530

    initial commit

--- loan_saathi_hub/urls.py
+++ loan_saathi_hub/urls.py
@@ -15,13 +15,15 @@ urlpatterns = [
     # ------------------------
     path("login/", views.login_view, name="login"),     # user + agent login
     path("dashboard/user/", views.dashboard_user, name="dashboard_user"),
-    path("profile/user/", views.complete_profile_user, name="complete_profile_user"),
+    path("complete-profile-user/", views.complete_profile_user, name="complete_profile_user"),
+
 
     # ------------------------
     # AGENT FLOW
     # ------------------------
     path("dashboard/agent/", views.dashboard_agent, name="dashboard_agent"),
-    path("profile/agent/", views.complete_profile_agent, name="complete_profile_agent"),
+    path("complete-profile-agent/", views.complete_profile_agent, name="complete_profile_agent"),
+
 
     # ------------------------
     # COMMON

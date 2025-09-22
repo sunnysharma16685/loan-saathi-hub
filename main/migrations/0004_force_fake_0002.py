7edcbb41a8925e2bd7ff7fecff0e0061b7df877c
https://github.com/sunnysharma16685/loan-saathi-hub <75515374+sunnysharma16685@users.noreply.github.com>
2025-09-23 00:26:16 +0530

    initial commit

---
+++ main/migrations/0004_force_fake_0002.py
@@ -0,0 +1,26 @@
+from django.db import migrations
+from django.utils import timezone
+
+def force_mark_0002(apps, schema_editor):
+    from django.db import connection
+
+    with connection.cursor() as cursor:
+        # Insert only if not exists
+        cursor.execute("""
+            INSERT INTO django_migrations (app, name, applied)
+            SELECT 'main', '0002_add_status_field', %s
+            WHERE NOT EXISTS (
+                SELECT 1 FROM django_migrations
+                WHERE app='main' AND name='0002_add_status_field'
+            )
+        """, [timezone.now()])
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('main', '0001_initial'),  # NOTE: depend only on 0001
+    ]
+
+    operations = [
+        migrations.RunPython(force_mark_0002, migrations.RunPython.noop),
+    ]
